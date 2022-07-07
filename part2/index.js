fddb51a5d55b560da7c5e74c0d485036f57f2c6e
Alex <iliynsd@icloud.com>
2022-07-07 14:41:02 +0300

    finish part 2

--- part2/index.js
+++ part2/index.js
@@ -1,3 +1,9 @@
+/**
+ * @constructor
+ * @description creates object with methods add,substract,multiply, divide, which can be invoked in the chain style and a method calculate, which calculates the result
+ * @param {Number} calculateValue
+ * @returns {Object} 
+ */
 function Calculator(calculateValue){
     if(!new.target){
         throw new Error("Функция-конструктор должна вызываться через new");
@@ -5,92 +11,96 @@ function Calculator(calculateValue){
 
     let expression = [calculateValue];
 
-    
     this.add = (number) => {
-        expression.add('+');
-        expression.add(number);
+        expression.push('+');
+        expression.push(number);
         return this;
     }  
     this.substraсt = (number) => {
-        expression.add('-');
-        expression.add(number);
+        expression.push('-');
+        expression.push(number);
         return this;
     }
     this.multiply = (number) => {
-        expression.add('*');
-        expression.add(number);
+        expression.push('*');
+        expression.push(number);
         return this;
     }
     this.divide = (number) => {
-        expression.add('/');
-        expression.add(number);
+        expression.push('/');
+        expression.push(number);
         return this;
     }
-}
 
-var calc = new Calculator(5);
+    const allOperations = ["+", "-", "*", "/"];
+    const highPriorityOperations = ["*", "/"];
 
-var calcString = ""
-     function calculation(buffString) {
-         var polishString = new Array;
-         var polishStack = new Array;
-         var stringId = -1;
-         var stackId = -1;
-         for (var i = 0; i < buffArray.length; i++) {                // формируем обратную польскую запись
-             switch (buffArray[i]) {
-              case "+":
-                  while (stackId >= 0 && (polishStack[stackId] == "+" || polishStack[stackId] == "-" || polishStack[stackId] == "*" || polishStack[stackId] == "/")) {
-                      stringId++;
-                      polishString[stringId] = polishStack[stackId];
-                      stackId--;
-                  }
-                  stackId++;
-                  polishStack[stackId] = buffArray[i];
-                  break;
-              case "-":
-                  while (stackId >= 0 && (polishStack[stackId] == "+" || polishStack[stackId] == "-" || polishStack[stackId] == "*" || polishStack[stackId] == "/")) {
-                      stringId++;
-                      polishString[stringId] = polishStack[stackId];
-                      stackId--;
-                  }
-                  stackId++;
-                  polishStack[stackId] = buffArray[i];
-                  break;
-              case "*":
-                  while (stackId >= 0 && (polishStack[stackId] == "*" || polishStack[stackId] == "/")) {
-                      stringId++;
-                      polishString[stringId] = polishStack[stackId];
-                      stackId--;
-                  }
-                  stackId++;
-                  polishStack[stackId] = buffArray[i];
-                 break;
-             case "/":
-                 while (stackId >= 0 && (polishStack[stackId] == "*" || polishStack[stackId] == "/")) {
+    this.calculation = () => {
+        var polishRecord = createPolishRecord();
+        return readPolishRecord(polishRecord.polishStack, polishRecord.polishString);
+    }
+
+    
+    /**
+     *@description This method creates a Polish record from expression to calculate
+     *@returns {Object} object has to fields polishString and polishStack
+     */
+    function createPolishRecord(){
+        var polishString = new Array;
+        var polishStack = new Array;
+        let stringId = -1;
+        let stackId = -1;
+        for (let i = 0; i < expression.length; i++) {                // формируем обратную польскую запись
+            switch (expression[i]) {
+             case "+":
+             case "-":
+                 while (stackId >= 0 && (allOperations.includes(polishStack[stackId]))) {
                      stringId++;
                      polishString[stringId] = polishStack[stackId];
                      stackId--;
                  }
                  stackId++;
-                 polishStack[stackId] = buffArray[i];
+                 polishStack[stackId] = expression[i];
                  break;
-             default:
-                 stringId++;
-                 polishString[stringId] = buffArray[i];
-           }
-        }
-        while (stackId >= 0) {
-            stringId++;
-            polishString[stringId] = polishStack[stackId];
-            stackId--;
-        }                                                                  // польская запись готова
-     //   alert(polishString);
-        stackId = -1;                                                      // Начинаем считать по польской записи
-        var stringIdMax = stringId;
+             
+             case "*":
+             case "/":
+                 while (stackId >= 0 && (highPriorityOperations.includes(polishStack[stackId]))) {
+                     stringId++;
+                     polishString[stringId] = polishStack[stackId];
+                     stackId--;
+                 }
+                 stackId++;
+                 polishStack[stackId] = expression[i];
+                break;
+            
+            default:
+                stringId++;
+                polishString[stringId] = expression[i];
+          }
+       }
 
-        for (stringId = 0; stringId <= stringIdMax; stringId++ ) {
-            switch (polishString[stringId]) {
-                case "+":
+       while (stackId >= 0) {
+           stringId++;
+           polishString[stringId] = polishStack[stackId];
+           stackId--;
+       }
+
+       return {polishString, polishStack};
+    }
+
+    /**
+     * @description This method reads polish record and return a result of calculated expression
+     * @param {Array} polishStack 
+     * @param {Array} polishString 
+     * @returns {Number}
+     */
+    function readPolishRecord(polishStack, polishString){
+        let stackId = -1;                                                  
+       
+        for (let i = 0; i < expression.length; i++ ) {
+            switch (polishString[i]) {
+                case "+":                
                     stackId--;
                     polishStack[stackId] = polishStack[stackId] + polishStack[stackId + 1];
                     break;
@@ -108,10 +118,14 @@ var calcString = ""
                     break;
                 default:
                     stackId++;
-                    polishStack[stackId] = parseFloat(polishString[stringId]);
-            }
-        }
-        return polishStack[stackId];
-     }
+                    polishStack[stackId] = parseFloat(polishString[i]);
+           }
+       }
 
-     console.log(calculation("1 + 2 * 3 / (4+5) * (6+7)"));
+       return polishStack[stackId];
+    }
+}
+var calc = new Calculator(5);
+calc.add(3).multiply(5).divide(2).substraсt(4).multiply(2);
+
+console.log(calc.calculation());
